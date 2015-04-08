e266c7162cf153df20b1c81c09bbb4cbe46be6af
Antoine Rabany <voctro@gmx.fr>
2015-04-08 15:45:28 +0200

    ajout des fonctionnalités liés a pyserial.

--- Application.py
+++ Application.py
@@ -6,6 +6,7 @@ matplotlib.use('TkAgg')
 
 import tkinter as tk
 from tkinter import ttk
+import serial
 
 from numpy import arange, sin, pi
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg, NavigationToolbar2TkAgg
@@ -28,21 +29,56 @@ class Application(ttk.Frame):
         self.tracer = ttk.Button(self, text="Tracer",
             command=self.tracer).grid(column=1, row=1)
 
-        self.QUIT = tk.Button(self, text="Quitter", fg="red",
+        self.QUIT = ttk.Button(self, text="Quitter",
             command=root.destroy).grid(column=2, row=1)
 
     def tracer(self):
         f = Figure(figsize=(5,4), dpi=100)
         a = f.add_subplot(111)
-        t = arange(0.0,3.0,0.01)
-        s = sin(2*pi*t)
 
-        a.plot(t,s)
+        self.capteur()
+        print(self.resultats)
+        print(len(self.resultats))
+        a.plot(self.t,self.resultats)
+        self.canvas = FigureCanvasTkAgg(f, master=root)
 
-        canvas = FigureCanvasTkAgg(f, master=root)
-        canvas.show()
+        self.canvas.show()
+        self.canvas.get_tk_widget().pack()
+
+
+        #self.canvas = FigureCanvasTkAgg(f, master=root)
+        #self.canvas.show()
         #canvas.get_tk_widget().pack(side=tk.TOP, fill=tk.BOTH, expand=1)
-        canvas.get_tk_widget().pack()
+        #self.canvas.get_tk_widget().pack()
+
+    def capteur(self):
+        baudrate = 115200
+        meusures = 2088
+        a=1
+        b=1
+        self.resultats=[]
+        ser = serial.Serial('/dev/ttyACM1', baudrate)
+        self.t=range(meusures)
+        if(ser.readline()==b'A\r\n'):
+            ser.write(b'1')
+            print('Démarage des meusures')
+            for i in self.t:
+                self.resultats.append(int(ser.readline().decode().rstrip('\r\n')))
+            print('meusures terminées')
+        #On enlève les meusures fausses
+        del self.resultats[0:32]
+        del self.resultats[len(self.resultats)-8:len(self.resultats)]
+        self.t=range(len(self.resultats))
+
+        #On normalise
+        self.resultats=[-x for x in self.resultats]
+        mini = min(self.resultats)
+        self.resultats=[x - mini for x in self.resultats]
+        maxi = max(self.resultats)
+        self.resultats=[x/maxi for x in self.resultats]
+
+
+
 
 root = tk.Tk()
 app = Application(master=root)
