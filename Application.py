ee2def58080a40c4c2cf9c12c3baac3e94fb6aaf
Antoine Rabany <voctro@gmx.fr>
2015-04-16 11:47:37 +0200

    suite

--- Application.py
+++ Application.py
@@ -9,18 +9,22 @@ from tkinter import ttk
 import serial
 
 from Capteur import Capteur
+from Converter import wav2RGB
 
-from numpy import arange, sin, pi
+from numpy import arange, sin, pi, outer, ones, array
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg, NavigationToolbar2TkAgg
 # implement the default mpl key bindings
 from matplotlib.backend_bases import key_press_handler
 import matplotlib.pyplot as plt
+import matplotlib.colors as col
+
+import colorpy
 
 from matplotlib.figure import Figure
 
 class Application(ttk.Frame):
     def __init__(self, master=None):
-        ttk.Frame.__init__(self, master, padding="2 2 12 12")
+        ttk.Frame.__init__(self, master, padding="3 2 12 12")
         master.title("Spectromètre")
         self.capteur = Capteur()
         self.grid(column=0, row=0)
@@ -30,7 +34,8 @@ class Application(ttk.Frame):
         self.createWidgets()
 
         self.f = Figure(figsize=(5,4), dpi=100)
-        self.ax = self.f.add_subplot(111)
+        self.ax = self.f.add_subplot(211)
+        self.ax2 = self.f.add_subplot(212,axisbg='k')
         self.canvas = FigureCanvasTkAgg(self.f, master=root)
         self.canvas.show()
         self.canvas.get_tk_widget().pack()
@@ -39,17 +44,41 @@ class Application(ttk.Frame):
         self.tracer = ttk.Button(self, text="Tracer",
             command=self.tracer).grid(column=1, row=1)
 
+        self.calibrer = ttk.Button(self, text="Calibrer",
+            command=self.calibrer).grid(column=2, row=1)
+
         self.QUIT = ttk.Button(self, text="Quitter",
-            command=root.destroy).grid(column=2, row=1)
+            command=root.destroy).grid(column=3, row=1)
 
     def tracer(self):
-
         self.ax.clear()
         #self.capteur.start()
         #self.ax.plot(self.capteur.t,self.capteur.resultats)
 
-        self.ax.plot(arange(0.0,3.0,0.01),sin(2*pi*arange(0.0,3.0,0.01)))#,color=plt.get_cmap('gist_rainbow'))
+        self.ax.plot(arange(380,750,1),abs(sin(2*pi*arange(380,750,1)/200)))#,color=plt.get_cmap('gist_rainbow'))
+        #res = abs(sin(2*pi*arange(380,780,1)))
+        res = []
+        for x in range(10):
+            a=[]
+            for y in arange(380,750):
+                a.append(wav2RGB(y,alpha=abs(sin(2*pi*y/200))))
+                #res[x,y] = wav2RGB(x)
+            res.append(a)
+
+                #res = outer(ones(100),[wav2RGB(x) for x in arange(380,780,1)])
+
+        #res = [wav2RGB(x) for x in arange(380,780,1)]
+        #res = outer(ones(100),arange(380,780))
+        #res = outer(ones(100),abs(sin(2*pi*arange(380,780,1)/200)))
+        #print(res)
+        #interp = 'bilinear';
+        #self.ax2.imshow(res,aspect='auto',cmap=plt.get_cmap('gist_rainbow'), extent=(350,800,0,1))
+        self.ax2.imshow(res,aspect='auto',extent=(350,750,0,1))
         self.canvas.draw()
+
+    def calibrer(self):
+        pass
+
 
 
 if __name__ == "__main__":
--- Capteur.py
+++ Capteur.py
@@ -6,21 +6,20 @@ class Capteur:
         self.baurate = 115200
         self.meusures = 2088
         self.resultats=[]
-        self.t=[]
+        self.tini=range(self.meusures)
+        self.t=range(2048)
 
-    def start1(self):
+    def start(self):
         ser = serial.Serial('/dev/ttyACM1', self.baudrate)
-        self.t=range(self.meusures)
         if(ser.readline()==b'A\r\n'):
             ser.write(b'1')
             print('Démarage des meusures')
-            for i in self.t:
+            for i in self.tini:
                 self.resultats.append(int(ser.readline().decode().rstrip('\r\n')))
             print('meusures terminées')
         #On enlève les meusures fausses
         del self.resultats[0:32]
         del self.resultats[len(self.resultats)-8:len(self.resultats)]
-        self.t=range(len(self.resultats))
 
         #On normalise
         self.resultats=[-x for x in self.resultats]
@@ -28,3 +27,6 @@ class Capteur:
         self.resultats=[x - mini for x in self.resultats]
         maxi = max(self.resultats)
         self.resultats=[x/maxi for x in self.resultats]
+
+    def calibrer(self):
+        pass
